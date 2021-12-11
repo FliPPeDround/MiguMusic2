583894550672c682832e67fc9e07b6ebe6bfa026
FliPPeDround <734243792@qq.com>
2021-12-11 20:35:02 +0800

    feat：添加newsongList页面

---
+++ src/components/HomeComps/newsongList.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-tabs>
+    <el-tab-pane label="华语">
+      <div class="song-box" v-for="(item, index) in MandarinSongs" :key="index">
+        <el-image :src="item.image" class="item-img" alt />
+        <div class="song-info">
+          <h3>{{ item.songName }}</h3>
+          <span
+            v-for="(item, index) in item.singers"
+            :key="index"
+          >
+          <span>{{ index > 0 ? ' / ' : '' }}</span>
+          <span>{{ item.singerName }}</span>
+          </span>
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="欧美">
+      <div class="song-box" v-for="(item, index) in WesternSongs" :key="index">
+        <el-image :src="item.image" class="item-img" alt />
+        <div class="song-info">
+          <h3>{{ item.songName }}</h3>
+          <p v-for="(item, index) in item.singers" :key="index">{{ item.singerName }}</p>
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="日韩">
+      <div class="song-box" v-for="(item, index) in JapKorSongs" :key="index">
+        <el-image :src="item.image" class="item-img" alt />
+        <div class="song-info">
+          <h3>{{ item.songName }}</h3>
+          <p v-for="(item, index) in item.singers" :key="index">{{ item.singerName }}</p>
+        </div>
+      </div>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+<script setup>
+import { useStore } from './../../store/home.js'
+import { computed } from 'vue'
+
+const store = useStore()
+const MandarinSongs = computed(() => store.newsongList[0]?.items.slice(0, 9))
+const WesternSongs = computed(() => store.newsongList[1]?.items.slice(0, 9))
+const JapKorSongs = computed(() => store.newsongList[2]?.items.slice(0, 9))
+
+const getsingers = (singers) => {
+  singers.map((item) => {
+    item.singerName
+  })
+}
+</script>
+
+<style>
+.el-tabs__nav {
+  float: right;
+}
+</style>
+
+<style lang="scss" scoped>
+.el-tab-pane {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  .song-box {
+    display: flex;
+    .item-img {
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,13 @@
   <h2>推荐歌单</h2>
   <songList/>
   <h2>新歌速递</h2>
+  <newsongList/>
 </template>
 
 <script setup>
 import banner from './../components/HomeComps/banner.vue'
 import songList from '../components/HomeComps/songList.vue'
+import newsongList from '../components/HomeComps/newsongList.vue'
 </script>
 
 <style>
@@ -15,5 +17,6 @@ h2 {
   text-align: left;
   color: #333;
   font-size: 24px;
+  margin: 1vw 0;
 }
 </style>

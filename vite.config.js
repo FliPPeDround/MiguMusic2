ec61e953873183c6cb322ca883db7f14d626abc8
FliPPeDround <734243792@qq.com>
2021-12-16 11:15:32 +0800

    fix：修复devtools报错位置与实际位置不同

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   base: "./",
+  build: {  
+    sourcemap: true
+  },  
   plugins: [
     vue(),
     Components({
